bc99a19d9398480b69968e78178f177b52fb955f
J4CKOP <53121879+J4CK0P@users.noreply.github.com>
2019-07-25 19:46:21 +0700

    Delete Tools.py

--- Tools.py
+++
@@ -1,6 +0,0 @@
-from requests import post 
-print "Coding By J4CKOP"
-i:input('masukan nomor:')
-dat = {'msisdn':i}
-re =post ("https://passport.jd.id/findPwd",dat)
-Print ("STATUS:",re.json()["message"])
