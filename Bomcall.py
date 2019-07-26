9d3abfebab9a6569fa22014be7c0e1930c0d77bd
J4CKOP <53121879+J4CK0P@users.noreply.github.com>
2019-07-26 18:10:14 +0700

    Create Bomcall.py

---
+++ Bomcall.py
@@ -0,0 +1,76 @@
+#!/system/xbin/bash
+clear
+git clone https://github.com/amsitlab/smsid-java.git
+cd ./smsid-java
+chmod +x ./install
+apt install ./smsid_1.1_all.deb
+clear
+
+toilet -f slant --gay "LiteSpam"
+sleep 1
+echo "\033[33;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[36;1m★""\033[34;1mSANTUY HACKER TEAM""\033[36;1m★"
+sleep 1
+echo "\033[35;1m1""\033[36;1mGrab"
+echo "\033[37;1m2.""\033[30;1mInformasi Tool Ini"
+echo "\033[31;1m0. Keluar"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Grab"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mJ4CKOP"
+sleep 5
+clear
+cd $HOME/J4CKOP/bomcall
+php Tools.php
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+toilet -f slant --gay "LiteSpam"
+echo "\033[34;1mContact:""\033[37;1m https://bit.ly/2yarlam "
+
+
+sleep 1
+echo "\033[35;1mPemograman:""\033[36;1m Bash"
+
+echo "\033[33;1mKarya: SHT"
+sleep 1
+echo "\033[32;1mVersi: v1"
+sleep 1
+echo "\033[36;1mTeam: SANTUY HACKER TEAM"
+sleep 1
+echo "\033[31;1mSubscribe YouTube""\033[37;1m https://www.youtube.com/channel/UCn82YGg7CnKcZh7QaNkLT8Q"
+echo "\033[35;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "SANTUY HACKER TEAM"
+sleep 1
+echo "HACKER INDONESIA"
+sleep 1
+
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+
+sh bommcall.sh
+fi
+
+
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
