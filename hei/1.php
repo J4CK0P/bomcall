51178245d447747ce3f36b7728a96743e64d3110
J4CKOP <53121879+J4CK0P@users.noreply.github.com>
2019-07-26 20:17:39 +0700

    Update 1.php

--- hei/1.php
+++ hei/1.php
@@ -2,16 +2,16 @@
 // Limit 3x Telpon Setiap Satu Nomor
 function send($phone){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://member-m.lazada.co.id/user/forget-password?spm=a2o4j.login_signup.login.dforgot_psw.674f52f59pgthe");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_URL, "https://my-m.lazada.co.id/member/verify?hybrid=1&redirect=https%3A%2F%2Fmember-m.lazada.co.id%2Fuser%2Freset-password&token=36cffd14-ef5f-40ac-ac5b-010973f2ac5d&type=COMMON_VERIFICATION");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=message");                        $asw = curl_exec($ch);
         curl_close($ch);
                 echo $asw."\n";
 }
-echo "COPYRIGHT ; SGBTEAM\n\n";
+echo "COPYRIGHT ; J4CK0P\n\n";
 echo "Nomor\nInput : ";
 $nomor = trim(fgets(STDIN));
 $execute = send($nomor);
